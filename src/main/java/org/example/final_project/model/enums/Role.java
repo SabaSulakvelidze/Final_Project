b5b4ad30df3004068690c1755c5900557ea49741
SabaSulakvelidze <SabaSulakvelidze@gmail.com>
2025-02-01 11:29:57 +0400

    added enums

---
+++ src/main/java/org/example/final_project/model/enums/Role.java
@@ -0,0 +1,10 @@
+package org.example.final_project.model.enums;
+
+import lombok.Getter;
+
+@Getter
+public enum Role {
+    ADMIN,
+    USER
+
+}
---
+++ src/main/java/org/example/final_project/model/enums/UserStatus.java
@@ -0,0 +1,7 @@
+package org.example.final_project.model.enums;
+
+public enum UserStatus {
+    ACTIVE,
+    INACTIVE,
+    CANCELED
+}
