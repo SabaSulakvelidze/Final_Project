95c46165b5870236c618767ee4116ba3374ab941
SabaSulakvelidze <SabaSulakvelidze@gmail.com>
2025-02-03 18:04:21 +0400

    added repos

--- src/main/java/org/example/final_project/repository/ProductRepository.java
+++ src/main/java/org/example/final_project/repository/ProductRepository.java
@@ -4,9 +4,6 @@ import org.example.final_project.model.entity.ProductEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import javax.xml.catalog.Catalog;
-
 @Repository
-public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
+public interface ProductRepository extends JpaRepository<ProductEntity,Long> {
 }
-
--- src/main/java/org/example/final_project/repository/StockRepository.java
+++ src/main/java/org/example/final_project/repository/StockRepository.java
@@ -1,9 +1,9 @@
 package org.example.final_project.repository;
 
-import org.example.final_project.model.entity.CatalogEntity;
+import org.example.final_project.model.entity.StockEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CatalogRepository extends JpaRepository<CatalogEntity, Long> {
+public interface StockRepository extends JpaRepository<StockEntity,Long> {
 }
