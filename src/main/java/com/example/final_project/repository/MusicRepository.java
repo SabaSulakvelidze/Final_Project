c16aa4b763407310db57e2af21a71a43aabd1c9f
Saba <sabasulakveidze@gmail.com>
2025-03-01 10:35:48 +0400

    add repos

---
+++ src/main/java/com/example/final_project/repository/MusicRepository.java
@@ -0,0 +1,14 @@
+package com.example.final_project.repository;
+
+import com.example.final_project.model.entity.MusicEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MusicRepository extends JpaRepository<MusicEntity, Long> {
+
+    Page<MusicEntity> findMusicEntitiesByMusicNameContaining(String musicName, Pageable pageable);
+
+}
