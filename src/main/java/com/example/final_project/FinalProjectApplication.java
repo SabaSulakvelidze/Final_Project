392127937337d1fa27cf2552bc4143be9de4f67e
Saba <sabasulakveidze@gmail.com>
2025-03-01 11:07:43 +0400

    add annotations to main method

--- src/main/java/com/example/final_project/FinalProjectApplication.java
+++ src/main/java/com/example/final_project/FinalProjectApplication.java
@@ -2,8 +2,14 @@ package com.example.final_project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
+@EnableWebSecurity
+@EnableScheduling
+@EnableAsync
 public class FinalProjectApplication {
 
     public static void main(String[] args) {
