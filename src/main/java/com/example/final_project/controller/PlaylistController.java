4924f3c5d48e40426c9cee3ab30b6177eae3c4c3
Saba <sabasulakveidze@gmail.com>
2025-03-05 14:08:05 +0400

    added PlaylistController & PlaylistFacade

---
+++ src/main/java/com/example/final_project/controller/PlaylistController.java
@@ -0,0 +1,80 @@
+package com.example.final_project.controller;
+
+import com.example.final_project.facade.PlaylistFacade;
+import com.example.final_project.model.request.PlaylistRequest;
+import com.example.final_project.model.response.PlaylistResponse;
+import com.example.final_project.model.response.PagedResponse;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Set;
+
+@RestController
+@RequestMapping("/playlist")
+@RequiredArgsConstructor
+public class PlaylistController {
+
+    private final PlaylistFacade playlistFacade;
+
+    @PostMapping("/addPlaylist")
+    @PreAuthorize("hasAnyRole('ADMIN','ARTIST','LISTENER')")
+    public ResponseEntity<PlaylistResponse> addPlaylist(@RequestBody @Valid PlaylistRequest playlistRequest) {
+        return new ResponseEntity<>(playlistFacade.addPlaylist(playlistRequest), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/getPlaylist/{playlistId}")
+    @PreAuthorize("hasAnyRole('ADMIN','ARTIST','LISTENER')")
+    public ResponseEntity<PlaylistResponse> getPlaylist(@PathVariable("playlistId") Long playlistId) {
+        return new ResponseEntity<>(playlistFacade.findPlaylistById(playlistId), HttpStatus.OK);
+    }
+
+    @GetMapping("/getPlaylists")
+    @PreAuthorize("hasAnyRole('ADMIN','ARTIST','LISTENER')")
+    public ResponseEntity<PagedResponse<PlaylistResponse>> getPlaylistEntities(
+            @RequestParam(required = false) String playlistName,
+            @RequestParam(required = false) String ownerName,
+
+            @RequestParam(defaultValue = "0") Integer pageNumber,
+            @RequestParam(defaultValue = "5") Integer pageSize,
+            @RequestParam(defaultValue = "ASC") Sort.Direction direction,
+            @RequestParam(defaultValue = "id") String sortBy) {
+        return new ResponseEntity<>(PagedResponse.of(playlistFacade.getPlaylists(
+                playlistName, ownerName,
+                pageNumber, pageSize, direction, sortBy)), HttpStatus.OK);
+    }
+
+    @PutMapping("/addMusicToPlaylist")
+    @PreAuthorize("hasAnyRole('ADMIN','ARTIST','LISTENER')")
+    public ResponseEntity<String> addMusicToPlaylist(@RequestParam Long playlistId,
+                                                     @RequestParam Set<Long> musicId) {
+        playlistFacade.addMusicToPlaylist(playlistId, musicId);
+        return ResponseEntity.ok().body("music with id %s added to playlist %s".formatted(musicId, playlistId));
+    }
+
+    @PutMapping("/removeMusicFromPlaylist")
+    @PreAuthorize("hasAnyRole('ADMIN','ARTIST','LISTENER')")
+    public ResponseEntity<String> removeMusicFromPlaylist(@RequestParam Long playlistId,
+                                                          @RequestParam Set<Long> musicId) {
+        playlistFacade.removeMusicFromPlaylist(playlistId, musicId);
+        return ResponseEntity.ok().body("music with id %s removed from playlist %s".formatted(musicId, playlistId));
+    }
+
+    @PutMapping("/update/{playlistId}")
+    @PreAuthorize("hasAnyRole('ADMIN','ARTIST','LISTENER')")
+    public PlaylistResponse updatePlaylistEntity(@PathVariable("playlistId") Long playlistId,
+                                                 @RequestBody PlaylistRequest playlistRequest) {
+        return playlistFacade.updatePlaylistById(playlistId, playlistRequest);
+    }
+
+    @DeleteMapping("/delete/{playlistId}")
+    @PreAuthorize("hasAnyRole('ADMIN','ARTIST','LISTENER')")
+    public ResponseEntity<Void> deletePlaylist(@PathVariable("playlistId") Long playlistId) {
+        playlistFacade.deletePlaylistById(playlistId);
+        return ResponseEntity.ok().build();
+    }
+}
---
+++ src/main/java/com/example/final_project/facade/PlaylistFacade.java
@@ -0,0 +1,78 @@
+package com.example.final_project.facade;
+
+import com.example.final_project.component.Utils;
+import com.example.final_project.model.entity.MusicEntity;
+import com.example.final_project.model.entity.PlaylistEntity;
+import com.example.final_project.model.entity.UserEntity;
+import com.example.final_project.model.request.PlaylistRequest;
+import com.example.final_project.model.response.PlaylistResponse;
+import com.example.final_project.service.MusicService;
+import com.example.final_project.service.PlaylistService;
+import com.example.final_project.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Set;
+
+@Service
+@RequiredArgsConstructor
+public class PlaylistFacade {
+    private final PlaylistService playlistService;
+    private final UserService userService;
+    private final MusicService musicService;
+
+    public PlaylistResponse addPlaylist(PlaylistRequest playlistRequest) {
+        UserEntity currentUser = userService.findUserById(Utils.getPrincipalDatabaseId());
+        PlaylistEntity playlistEntity = playlistService.save(PlaylistEntity.toPlayListEntity(playlistRequest, currentUser));
+        if (playlistRequest.getMusicIdList() != null && !playlistRequest.getMusicIdList().isEmpty())
+            playlistRequest.getMusicIdList().forEach(musicId -> playlistEntity.getMusicList().add(musicService.findMusicById(musicId)));
+        return PlaylistResponse.toPlaylistResponse(playlistEntity);
+    }
+
+    public Page<PlaylistResponse> getPlaylists(String playlistName, String ownerName,
+                                               Integer pageNumber, Integer pageSize, Sort.Direction direction, String sortBy) {
+        return playlistService.getPlaylists(
+                playlistName, ownerName,
+                pageNumber, pageSize, direction, sortBy).map(PlaylistResponse::toPlaylistResponse);
+    }
+
+    public PlaylistResponse findPlaylistById(Long playlistId) {
+        return PlaylistResponse.toPlaylistResponse(playlistService.findPlaylistById(playlistId));
+    }
+
+    @Transactional
+    public PlaylistResponse updatePlaylistById(Long playlistId, PlaylistRequest playlistRequest) {
+        Utils.checkIfCurrentUserIsOwner(playlistService.findPlaylistById(playlistId).getOwner().getUsername());
+        return PlaylistResponse.toPlaylistResponse(playlistService.updatePlaylistById(playlistId, PlaylistEntity.toPlayListEntity(playlistRequest, null)));
+    }
+
+    @Transactional
+    public void addMusicToPlaylist(Long playlistId, Set<Long> musicIds) {
+        Utils.checkIfCurrentUserIsOwner(playlistService.findPlaylistById(playlistId).getOwner().getUsername());
+        PlaylistEntity playlistEntity = playlistService.findPlaylistById(playlistId);
+        musicIds.forEach(musicId -> {
+            MusicEntity musicEntity = musicService.findMusicById(musicId);
+            playlistEntity.getMusicList().add(musicEntity);
+        });
+        playlistService.save(playlistEntity);
+    }
+
+    @Transactional
+    public void removeMusicFromPlaylist(Long playlistId, Set<Long> musicIds) {
+        Utils.checkIfCurrentUserIsOwner(playlistService.findPlaylistById(playlistId).getOwner().getUsername());
+        PlaylistEntity playlistEntity = playlistService.findPlaylistById(playlistId);
+        musicIds.forEach(musicId -> {
+            MusicEntity musicEntity = musicService.findMusicById(musicId);
+            playlistEntity.getMusicList().remove(musicEntity);
+        });
+        playlistService.save(playlistEntity);
+    }
+
+    public void deletePlaylistById(Long playlistId) {
+        playlistService.deletePlaylistIdById(playlistId);
+    }
+}
